959eca3770f2e4d6bdc22599672583e49fac0f19
Qingyu Zhou <zhouqingyu.rex@gmail.com>
2016-04-16 22:34:49 -0700

    add idea

---
+++ project1/src/pingPongTest/PingPongTestClient.java
@@ -0,0 +1,18 @@
+package client;
+
+import java.io.*;
+import java.net.*;
+
+import rmi.*;
+/*
+public class PingPongTestClient  {
+    public static void main(String [] args) {
+        InetSocketAddress address = new InetSOcketAddress('myServer',8888);
+        Server  server = Stub.create(Server.class, address);
+
+
+    }
+
+
+}
+*/
